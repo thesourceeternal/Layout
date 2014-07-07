1d89acdd8fabbe34b42f9a334ace94e5fa976b4b
knod <knodding@gmail.com>
2014-07-07 08:40:01 -0400

    Mostly experimenting with collapsiblity functionality. See below.
    - Experiments with collapsibility
    - Adds element to take up space for the sidebar tabs (floats)
    - Starts to transfer needed "reset" styles to sidebar.css
    - Adds section in sidebar.css for "functionality" (I believe)

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,41 @@
-// Make a function to cycle through all siblings and deactivate them?
+/*
+* The way collapsibility and the html are set up may not be ideal or standard,
+* no internet to refresh my memory right now. I have a feeling
+* there's a more elegant way to do this, though the fact that I want
+* to be able to highlight selected elements sometimes complicates matters.
+ */
+
+$(document).ready(function() {
+
+	// --- Expand and Collapse --- \\
+	$(".sidebar")
+		// Hide and show elements appearing nested
+		.on("click", ".collapser", function (evt) {
+			$clickedElem = $(this)
+			$clickedElem.parent().next().slideToggle();
+			// Toggle the arrow image
+			// $clickedElem.toggleClass("expanded");
+			// For accessibility, change the alt text of the arrow
+			if ($clickedElem.attr("alt") !== "Click to collapse") {
+				$clickedElem.attr("alt", "Click to collapse")
+			}
+   			else if ($clickedElem.attr("alt") !== "Click to expand") {
+				$clickedElem.attr("alt", "Click to expand")
+   			}
+
+			evt.stopPropagation();
+			evt.preventDefault();
+		});
+
+	// --- Select scene object in tree --- \\
+	$(".scene-tree")
+		.on("click", ".selectable", function (evt) {
+			$clickedElem = $(this)
+
+			// Deselect all elements
+			$(".scene-tree").find($(".selected-obj")).removeClass("selected-obj")
+			// Select this element
+			$clickedElem.addClass("selected-obj")
+		})
+
+});
